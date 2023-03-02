3e6d21fe1e3bfb248303814e4b4106d1c8f77825
IvanDominfpmoz <114880588+IvanDominfpmoz@users.noreply.github.com>
2023-03-02 10:16:32 +0100

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongodb = require('mongodb');
-const router = express.Router(); // router je komponenta express-a koja sluzi za odredjivanje rute ka komponentama 
+const router = express.Router(); // router je komponenta express-a koja sluzi za određivanje rute prema komponentama 
 
 
 
@@ -15,14 +15,14 @@ router.get('/', async (req, res) =>{
 router.post('/', async(req, res) =>{
   const posts = await loadPostsCollection();
   await posts.insertOne({ //insert one
-    apparel: req.body, //req.body je data koju dobija kad odradi request i smesta je u apparel
+    apparel: req.body, //req.body je podatak koji dobiva kad odradi request i smjesta ga u apparel
     createdAt:new Date() // datum
   });
   res.status(201).send();//salje signal 201 posiljaocu kao potvrda da je sve proslo ok
 })
 
 //Delete Post
-router.delete('/:id', async (req, res)=>{ // sve ove dalje funkcije su iste, samo se menja request znaci imas GET, POST, DELETE
+router.delete('/:id', async (req, res)=>{ // sve ove dalje funkcije su iste, samo se mijenja request to su GET, POST, DELETE
   const posts = await loadPostsCollection();
   await posts.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
   res.status(200).send()
